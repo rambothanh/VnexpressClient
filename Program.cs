21c9afe52ef1e154a39da5fe81a8739eadb12582
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-18 15:56:13 +0700

    edit BaseAddress

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace VnexpressClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/")});
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://todoapi.sofsog.com/api/")});
 
             await builder.Build().RunAsync();
         }
